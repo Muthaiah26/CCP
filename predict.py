5ce021b2344f63d7dc0b0ea7284e4d5c662ed955
Muthaiah26 <pandimuthaiah2006@gmail.com>
2025-09-20 12:03:33 +0530

    deleted

--- predict.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd, joblib
-
-model = joblib.load("models/maintenance_model.pkl")
-new_data = pd.DataFrame([[110, 1.6]], columns=["temperature", "vibration"])
-prediction = model.predict(new_data)[0]
-
-print("⚠ Maintenance Required!" if prediction == 1 else "✅ Machine Healthy")
